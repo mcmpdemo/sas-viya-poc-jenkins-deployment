310b7eba29a6146fcdf77e5d3453467de53e90ff
Jose Pablo Ramirez Mendez <jose.ramirez.mendez1@kyndryl.com>
2022-04-22 13:47:27 -0600

    NEW: adding sas portal token auth

--- create_input_files.py
+++ create_input_files.py
@@ -6,16 +6,9 @@ import yaml
 NAMESPACE = "viya-4-sas-ns"
 
 def get_order_details(user_name,api_key,order_number,tenant_url):
-    
     ENDPOINT = "{0}/v3/api/services/azure/{1}".format(tenant_url, order_number)
-
-    headers = {
-        'username': user_name,
-        'apikey': api_key
-    }   
-
+    headers = { 'username': user_name, 'apikey': api_key}   
     response = requests.get(url=ENDPOINT, headers=headers)
-
     return response.json()
 
 def create_terraform_file(raw_data):
@@ -30,14 +23,29 @@ def create_ansible_vars_template(raw_data):
     ansible_vars_file.write(decoded)
     ansible_vars_file.close()
 
+def get_sas_portal_token(api_key,api_secret):
+    
+    SCOPES = "getCertificates getDepAssets getDepAssetsShort getLicense"
+    ENDPOINT = "https://api.sas.com/mysas/token?grant_type=client_credentials&client_id={0}&client_secret={1}&scope={2}"
+
+    response = requests.post( url=ENDPOINT.format(api_key, api_secret,SCOPES) )
+
+    print(response.status_code)
+    print(response.text)
+    
+
 if __name__ == "__main__":
 
     USER_NAME = sys.argv[1]
     API_KEY = sys.argv[2]
     ORDER_NUMBER = sys.argv[3]
     TENANT_API_URL = sys.argv[4]
+    
     TF_STATE_FILE = sys.argv[5]
     ANSIBLE_VARS_TEMPLATE = sys.argv[6]
+    
+    V4_CFG_SAS_API_KEY = sys.argv[7]
+    V4_CFG_SAS_API_SECRET = sys.argv[8]
 
     print("Getting order details...")
     order_details = get_order_details(USER_NAME, API_KEY, ORDER_NUMBER, TENANT_API_URL)
@@ -47,4 +55,7 @@ if __name__ == "__main__":
 
     print("Creating ansible vars template file...")
     create_ansible_vars_template(ANSIBLE_VARS_TEMPLATE)
+
+    print("Get sas portal token...")
+    get_sas_portal_token(V4_CFG_SAS_API_KEY, V4_CFG_SAS_API_SECRET)
 
