0ab498d4b30a1289b2cd074b89849b8b0e29076a
Jose Pablo Ramirez Mendez <jose.ramirez.mendez1@kyndryl.com>
2022-04-27 10:13:25 -0600

    NEW: adding script to create kubeconfig file

--- extract_kubeconfig_file.py
+++ extract_kubeconfig_file.py
@@ -1,7 +1,13 @@
 import json
 
 if __name__ == "__main__":
+    
     tf_state_file = open("terraform.tfstate")
     content = tf_state_file.read()
+    tf_state_file.close()
     data = json.loads(content)
-    print(data)
+    
+    kube_config_data = data["outputs"]["kube_config"]["value"]
+    kube_config_file = open("kubeconfig","w")
+    kube_config_file.write(kube_config_data)
+    kube_config_file.close()
